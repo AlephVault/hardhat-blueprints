3a11a149c5d6bcf66eefb1d49effae2c4b810a0d
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-07 20:39:38 -0500

    Adding the prompts file.

---
+++ src/prompts.js
@@ -0,0 +1,67 @@
+let prompts = {
+    "contractName": {
+        type: "plus:given-or-valid-input",
+        validate: /^[A-Z][A-Za-z0-9_]*$/,
+        makeInvalidInputMessage: (v) => `Invalid contract name: ${v}`,
+        onInvalidGiven: (v) => console.error(`Invalid given contract name: ${v}`)
+    },
+    "identifier": {
+        type: "plus:given-or-valid-input",
+        validate: /^[a-z][A-Za-z0-9_]*$/,
+        makeInvalidInputMessage: (v) => `Invalid identifier: ${v}`,
+        onInvalidGiven: (v) => console.error(`Invalid given identifier: ${v}`)
+    },
+    "number": {
+        type: "plus:given-or-valid-number-input",
+        convert: "string"
+    },
+    "integer": {
+        type: "plus:given-or-valid-number-input",
+        integerOnly: true, allowHex: true,
+        convert: "string"
+    },
+    "boolean": {
+        type: "plus:given-or-boolean-select",
+    },
+    "address": {
+        type: "plus:hardhat:given-or-valid-address-input",
+        allowAccountIndex: false
+    },
+    "smart-address": {
+        type: "plus:hardhat:given-or-valid-address-input",
+        allowAccountIndex: true
+    }
+}
+
+/**
+ * Prepares a prompt. It picks a prompt type (or takes it as-is)
+ * and adds the other arguments: name, message and given.
+ * @param name The internal name of the argument.
+ * @param message The display message.
+ * @param given The initial given value (optional).
+ * @param promptType The prompt type. Either one of the registered
+ * prompt types.
+ * @returns {*} The prompt entry.
+ */
+function preparePrompt(name, message, given, promptType) {
+    // First, the prompt type is either a textual/registered string
+    // or a partial prompt object. Then, the other members are added.
+    return {name, message, given, ...(prompts[promptType] || promptType)};
+}
+
+/**
+ * Registers a prompt type for the arguments.
+ * @param promptType The name of the prompt type.
+ * @param promptTypeSpec The object depicting a prompt (in the same
+ * format that would be provided for enquirer's prompt() method).
+ */
+function registerPromptType(promptType, promptTypeSpec) {
+    if (prompts[promptType] !== undefined) {
+        throw new Error(`A prompt type is already registered with this name: ${promptType}`);
+    }
+    prompts[promptType] = promptTypeSpec;
+}
+
+module.exports = {
+    preparePrompt, registerPromptType
+}
