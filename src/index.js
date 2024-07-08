240d932c6e12af62c82184f003d54ab7c3c2f9b4
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-07 20:53:21 -0500

    Starting the blueprints part: registerBlueprint.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 const {scope} = require("hardhat/config");
 
+// The list of blueprints. Each one has {defaultName, filePath, arguments}.
+let blueprints = {};
 
+// The elements to render to pick a contract.
+let blueprintsList = [];
+
+/**
+ * Registers a blueprint.
+ * @param key The internal name of the contract entry. It must be unique.
+ * @param title The title/description for the option.
+ * @param defaultName The default name for the contract.
+ * @param filePath The path to the template file.
+ * @param arguments The list of arguments. Each one must be an entry like
+ * this: {name, message, promptType} where name and message will directly
+ * be forwarded to `enquirer` prompts, while the promptType will be either
+ * a registered preset (e.g. string, number, contract, boolean, address or
+ * smart address, or others that may come across later) or a one-off prompt
+ * entry (which completely would match an entry in a call to enquirer's
+ * prompt() method).
+ */
+function registerBlueprint(key, defaultName, title, filePath, arguments) {
+    if (blueprints[key]) {
+        throw new Error(`Blueprint key already registered: ${key}`);
+    }
+    blueprintsList.push({name: key, message: title});
+    blueprints[key] = {defaultName, filePath, arguments};
+}
 
 module.exports = {
-
+    registerBlueprint
 }
